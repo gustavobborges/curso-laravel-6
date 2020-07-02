0802bea4f6cb5d2d7d8c83ae57c149919bcd56b2
Borges <gustavoborges98@hotmail.com>
2020-07-02 16:39:21 -0300

    Routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,38 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::view('/view', 'welcome');
+
+Route::redirect('/redirect1', '/redirect2');
+
+// Route::get('redirect1', function() {
+//     return redirect('/redirect2');
+// });
+
+Route::get('redirect2', function() {
+    return 'Redirect 02';
+});
+
+Route::get('/produtos/{idProduct?}', function($idProduct = ''){
+    return "Produto(s) {$idProduct}";
+});
+
+Route::get('/categorias/{flag}/posts', function($flag) {
+    return "Posts da caterogira: {$flag}";
+});
+
+Route::get('/categorias/{flag}', function($prm1) {
+    return "Produtos da caterogira: {$prm1}";
+});
+
+Route::match(['get', 'post'], '/match', function() {
+    return 'match';
+});
+
+Route::any('/any', function() {
+    return 'Any';
+});
+
 Route::post('/register ', function() {
     return '';
 });
