1551f196e952aeb519f1c2db7d48279b09f97cfc
Borges <gustavoborges98@hotmail.com>
2020-07-02 18:36:51 -0300

    Passando parâtros pelas rotas para o Controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,4 +12,9 @@ class ProductController extends Controller
 
         return $products;
     }
+
+    public function show($id)
+    {
+        return "Exibindo o produto de id: {$id}";
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('products/{id}', 'ProductController@show')->name('products.show');
+
 Route::get('products', 'ProductController@index')->name('products.index');
 
 Route::get('/login', function() {
