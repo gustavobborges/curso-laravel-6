6d749c83c75ec0bbe1e980ec7b6c3947d9c89dd0
Borges <gustavoborges98@hotmail.com>
2020-07-02 19:01:52 -0300

    Criação do controller automaticamente com --resource

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,40 +6,79 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index() 
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
     {
-        $products = ['Product 01', 'Product 02', 'Product 03'];
-
-        return $products;
+        //
     }
 
-    public function show($id)
-    {
-        return "Exibindo o produto de id: {$id}";
-    }
-
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function create()
     {
-        return "Exibindo o form de cadastro de um novo produto.";
+        //
     }
 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function edit($id)
     {
-        return "Editar o produto de id: {$id}.";
+        return "editando...";
     }
 
-    public function store()
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
     {
-        return "Cadastrando um novo produto.";
+        //
     }
 
-    public function update($id)
-    {
-        return "Editar o produto : {$id}.";
-    }
-
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function destroy($id)
     {
-        return "Deletar o produto : {$id}.";
+        //
     }
 }
