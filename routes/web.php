e2abcb73ac674d9305a098d625259db7d5c55472
Borges <gustavoborges98@hotmail.com>
2020-07-02 18:06:46 -0300

    Finalização do assunto de Rotas - Até aula 15

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,57 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/login', function() {
+    return 'login';
+})->name('login');
+
+
+// Route::middleware([])->group(function() {
+//     Route::prefix('admin')->group(function() {
+//         Route::namespace('Admin')->group(function () {
+//             Route::name('admin.')->group(function() {
+//                 Route::get('/dashboard', 'TesteController@teste')->name('dashboard');
+        
+//                 Route::get('/financeiro', 'TesteController@teste')->name('financeiro');
+                
+//                 Route::get('/produtos', 'TesteController@teste')->name('produtos');  
+        
+//                 Route::get('/', function() {
+//                     return redirect()->route('home');
+//                 });
+//             });  
+//         });      
+//     });
+// });
+
+Route::group([
+    'middleware' => [],
+        'middleware' => [],
+        'prefix' => 'admin',
+        'namespace' => 'Admin',
+        'name' => 'admin.'  
+], function(){
+    Route::get('/dashboard', 'TesteController@teste')->name('dashboard');
+        
+    Route::get('/financeiro', 'TesteController@teste')->name('financeiro');
+    
+    Route::get('/produtos', 'TesteController@teste')->name('produtos');  
+
+    Route::get('/', function() {
+        return redirect()->route('admin.dashboard');  
+    })->name('home');
+});
+
+Route::get('redirect3', function() {
+    return redirect()->route('url.name');
+});
+
+// route('/nome-url');
+
+Route::get('/nome-url', function() {
+    return 'Hey Hey Lets Go Pikachu';
+})-> name('url.name');
+
 Route::view('/view', 'welcome');
 
 Route::redirect('/redirect1', '/redirect2');
@@ -45,3 +96,5 @@ Route::get('/contato', function() {
 Route::get('/', function () {
     return view('welcome');
 });
+
+
