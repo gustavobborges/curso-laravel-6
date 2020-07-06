01c6741e4225cd63d17ef5ddf21aa7f1ff3ed744
Borges <gustavoborges98@hotmail.com>
2020-07-06 14:16:56 -0300

    Estruturas de repetição Blade

--- resources/views/admin/pages/products/index.blade.php
+++ resources/views/admin/pages/products/index.blade.php
@@ -5,6 +5,21 @@
 @section('content')
     <h1>Exibindo os Produtos</h1>
 
+    @if (isset($products))
+        @foreach ($products as $product)
+            <p class="@if ($loop->last) last @endif"> {{ $product }} </p>
+        @endforeach
+    @endif
+
+    <hr>
+    @forelse ($products as $product)
+        <p class="@if ($loop->first) last @endif"> {{ $product }} </p>  
+    @empty
+        <p>Não existem produtos cadastrados</p>
+    @endforelse
+    <hr>
+
+
     @if ($teste === '123')
         É igual
     @elseif($teste === 123)
@@ -49,6 +64,12 @@
         @default
             Default
     @endswitch
+
+
+    <style>
+        .last {background: #CCC;}
+
+    </style>
 
 @endsection('content')
 
